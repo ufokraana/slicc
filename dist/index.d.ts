e4149b394e8bcc81532494faa3005b3d247152a9
Urmas Lamster <urmasl@gmail.com>
2019-05-12 18:42:12 +0300

    Removed dist from repo

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './bindPrimitives';
-export * from './combine';
-export * from './fromPromise';
-export * from './slice';
-export * from './store';
--- dist/store.d.ts
+++
@@ -1,3 +0,0 @@
-import { ISlice, StateFromSlice, ActionsFromSlice, IStore } from "./interface";
-declare const store: <Slice extends ISlice<any, any>, State extends StateFromSlice<Slice>, Actions extends ActionsFromSlice<Slice>>(slice: Slice) => IStore<State, Actions>;
-export default store;
